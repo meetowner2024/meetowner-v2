783d583e2e71ab5f042bcc210c30db203f737e79
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-09-05 19:03:07 +0530

    sd

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
-import JavaScriptObfuscator from "webpack-obfuscator";
-import CompressionPlugin from "compression-webpack-plugin";
+
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
   openAnalyzer: true,
 });
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
     ],
     minimumCacheTTL: 60 * 60 * 24,
   },
-  webpack(config, { dev, isServer }) {
+  webpack(config) {
     config.optimization.splitChunks.cacheGroups = {
       ...config.optimization.splitChunks.cacheGroups,
       dashboard: {
@@ -41,43 +41,11 @@ const nextConfig: NextConfig = {
         enforce: true,
       },
     };
-    if (!dev && !isServer) {
-      config.plugins.push(
-        new JavaScriptObfuscator(
-          {
-            compact: true,
-            controlFlowFlattening: true,
-            deadCodeInjection: true,
-            debugProtection: true,
-            disableConsoleOutput: true,
-            identifierNamesGenerator: "hexadecimal",
-            selfDefending: true,
-          },
-          ["vendor.js"]
-        )
-      );
-      config.plugins.push(
-        new CompressionPlugin({
-          algorithm: "brotliCompress",
-          filename: "[path][base].br",
-          test: /\.(js|css|html|svg)$/,
-          compressionOptions: { level: 11 },
-          threshold: 10240,
-          minRatio: 0.8,
-        }),
-        new CompressionPlugin({
-          algorithm: "gzip",
-          filename: "[path][base].gz",
-          test: /\.(js|css|html|svg)$/,
-          threshold: 10240,
-          minRatio: 0.8,
-        })
-      );
-    }
     return config;
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
 export default withBundleAnalyzer(nextConfig);
